e64f05118ba638b9b6e084c35c082d6a65503613
Adele Martin <ademarti@c1b1c2.42berlin.de>
2024-02-05 13:57:33 +0100

    ok

--- parsing.c
+++ parsing.c
@@ -6,13 +6,43 @@
 /*   By: ademarti <ademarti@student.42berlin.de     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 16:53:18 by ademarti          #+#    #+#             */
-/*   Updated: 2024/02/02 17:45:08 by ademarti         ###   ########.fr       */
+/*   Updated: 2024/02/05 13:57:16 by ademarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int read_map (, char **argv)
-{
+// int check_map (char *map, t_game **argv)
+// {
 
-}
+// }
+
+// int	read_map(t_complete *game, char **argv)
+// {
+// 	char	*readmap;
+
+// 	game->fd = open(argv[1], O_RDONLY);
+// 	if (game->fd < 0)
+// 		return (0);
+// 	while (1)
+// 	{
+// 		readmap = get_next_line(game->fd);
+// 		if (!add_line(game, readmap))
+// 			break ;
+// 	}
+// 	close (game->fd);
+// 	game->widthmap = width_of_map(game->map[0]);
+// 	return (1);
+// }
+
+// int	read_map(char *map)
+// {
+// 	int fd;
+
+// 	fd = open(map, O_RDWR);
+// 	if (fd == -1)
+// 	{
+// 		ft_printf("");
+// 		return (1);
+// 	}
+// }
